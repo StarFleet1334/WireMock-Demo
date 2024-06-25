92062051060e30f9a0f73fe091546c2859fc8031
IliaLataria01 <latariailia6@gmail.com>
2024-06-25 19:43:35 +0400

    Add functionality of GET,POST,PUT,DELETE with test cases

--- data-app/src/main/java/com/demo/folder/service/AnimeRestClient.java
+++ data-app/src/main/java/com/demo/folder/service/AnimeRestClient.java
@@ -1,7 +1,6 @@
 package com.demo.folder.service;
 
 import com.demo.folder.dto.Anime;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 
 import java.util.List;
 
-import static com.demo.folder.endpoints.UtilEndpoints.GET_ALL_ANIME;
+import static com.demo.folder.endpoints.UtilEndpoints.*;
 
 public class AnimeRestClient {
     private static final Logger LOGGER = LoggerFactory.getLogger(AnimeRestClient.class);
@@ -33,24 +32,68 @@ public class AnimeRestClient {
     }
 
     public Anime getAnimeById(Integer id) {
-
-
-        return null;
+        Anime anime = null;
+        try {
+            anime = webClient.get().uri(GET_ANIME_BY_ID, id)
+                    .retrieve()
+                    .bodyToMono(Anime.class)
+                    .block();
+        } catch (WebClientResponseException ex) {
+            LOGGER.error("WebClientResponseException - Error Message is : {} ", ex.getResponseBodyAsString());
+        } catch (Exception ex) {
+            LOGGER.error("Exception - The Error Message is {} ", ex.getMessage());
+        }
+        return anime;
     }
 
-    public Anime  postNewAnime(Anime anime) {
-
-
-        return null;
+    public Anime postNewAnime(Anime newAnime) {
+        Anime anime = null;
+        try {
+            anime = webClient.post().uri(POST_ANIME_BY_ID)
+                    .syncBody(newAnime)
+                    .retrieve()
+                    .bodyToMono(Anime.class)
+                    .block();
+            LOGGER.info("New Movie SuccessFully addded {} ", anime);
+        } catch (WebClientResponseException ex) {
+            LOGGER.error("WebClientResponseException - Error Message is : {} , and the Error Response Body is {}", ex, ex.getResponseBodyAsString());
+        } catch (Exception ex) {
+            LOGGER.error("Exception - The Error Message is {} ", ex.getMessage());
+        }
+        return anime;
     }
 
     public Anime updateExistingAnime(Integer animeId, Anime anime) {
+        Anime updatedAnime = null;
 
-        return null;
+        try {
+            updatedAnime = webClient.put().uri(PUT_ANIME_BY_ID, animeId)
+                    .syncBody(anime)
+                    .retrieve()
+                    .bodyToMono(Anime.class)
+                    .block();
+            LOGGER.info(" Movie SuccessFully updated {} ", updatedAnime);
+        } catch (WebClientResponseException ex) {
+            LOGGER.error("WebClientResponseException - Error Message is : {}", ex.getResponseBodyAsString());
+        } catch (Exception ex) {
+            LOGGER.error("Exception - The Error Message is {} ", ex.getMessage());
+        }
+
+        return updatedAnime;
     }
 
     public String deleteAnimeById(Integer animeId) {
-
-        return null;
+        String response = "";
+        try {
+            response = webClient.delete().uri( DELETE_ANIME_BY_ID, animeId)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+        }catch (WebClientResponseException ex) {
+            LOGGER.error("WebClientResponseException - Error Message is : {}",ex.getResponseBodyAsString());
+        } catch (Exception ex) {
+            LOGGER.error("Exception - The Error Message is {} ", ex.getMessage());
+        }
+        return response;
     }
 }
--- data-app/src/test/java/com/demo/folder/AnimeRestTest.java
+++ data-app/src/test/java/com/demo/folder/AnimeRestTest.java
@@ -2,15 +2,16 @@ package com.demo.folder;
 
 import com.demo.folder.dto.Anime;
 import com.demo.folder.service.AnimeRestClient;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AnimeRestTest {
     AnimeRestClient moviesRestClient = null;
@@ -33,4 +34,70 @@ public class AnimeRestTest {
         //then
         assertFalse(movieList.isEmpty());
     }
+
+    @Test
+    void retrieveAnimeById() {
+        //given
+        Integer animeId = 2;
+
+        //when
+        Anime anime = moviesRestClient.getAnimeById(animeId);
+
+        LOGGER.info("Anime retrieved: {}", anime);
+        //then
+        assertEquals("Naruto", anime.getTitle());
+    }
+
+
+    @Test
+    void addNewAnime() {
+        //given
+        Anime newAnime =  new Anime();
+        newAnime.setTitle("Kavawaki");
+        newAnime.setRating(9.2);
+        newAnime.setMaincharacter("Bob");
+        newAnime.setDescription("It is long story");
+
+        //when
+        Anime anime = moviesRestClient.postNewAnime(newAnime);
+
+        //then
+        assertTrue(anime.getTitle() != null);
+
+    }
+
+
+
+    @Test
+    void updateMovie() {
+        //given
+        Anime updatedAnime = new Anime(3, "One Piece (Found)", 9, "Monkey D. Luffy", "Luffy and his pirate crew in search of the legendary treasure called \"One Piece\" to become the Pirate King.");
+
+        Integer animeId = 3;
+
+        //when
+        Anime anime = moviesRestClient.updateExistingAnime(animeId, updatedAnime);
+
+        //then
+        String newTitle = "One Piece (Found)";
+        assertTrue(anime.getTitle().contains(newTitle));
+    }
+
+
+    @Test
+    void deleteMovie() {
+
+        //given
+        String batmanBeginsCrew = "Tom Hanks, Tim Allen";
+        Anime anime = new Anime(4, "My Hero Academia", 8.8, "Izuku Midoriya", "A boy born without superpowers in a world where they are common, but still dreams of becoming a superhero.");
+
+
+        //when
+        String response = moviesRestClient.deleteAnimeById(4);
+        LOGGER.info("\nAnime response: {}", response);
+    }
+
+
+
+
 }
